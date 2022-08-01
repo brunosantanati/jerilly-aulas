0f3760fc4a922d16d30e9bb1959227ff28f5e819
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-08-01 09:15:42 -0300

    Codigo de classes e objetos.

--- primeira-aula/src/main/java/org/example/App.java
+++ primeira-aula/src/main/java/org/example/App.java
@@ -4,10 +4,24 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+        Casa minhaCasa = new Casa();
+        minhaCasa.numeroDeComodos = 6;
+        minhaCasa.cor = "azul";
+
+        System.out.println(minhaCasa);
+
+        Casa casaDaJerilly = new Casa();
+        casaDaJerilly.numeroDeComodos = 4;
+        casaDaJerilly.cor = "branca";
+
+        System.out.println(casaDaJerilly);
     }
 }
---
+++ primeira-aula/src/main/java/org/example/Casa.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Casa {
+
+    int numeroDeComodos;
+    String cor;
+
+    @Override
+    public String toString() {
+        return "Casa{" +
+                "numeroDeComodos=" + numeroDeComodos +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+}
